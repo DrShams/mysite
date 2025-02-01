5f526f0c684e7b7e2f0dcc6491db902e30e8ca10
DrShams <sithis02@mail.ru>
2025-02-01 14:13:59 +0300

    ruslan

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.static import serve
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 
 # Up two folders to serve "site" content
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -13,7 +14,8 @@ urlpatterns = [
     path('polls/', include('polls.urls')),
     path('hello/', include('hello.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('autos/', include('autos.urls')),                                                                                   
+    path('autos/', include('autos.urls')),
+    path("change-password/", auth_views.PasswordChangeView.as_view()),                                                                                 
     re_path(r'^site/(?P<path>.*)$', serve,
         {'document_root': SITE_ROOT, 'show_indexes': True},
         name='site_path'
