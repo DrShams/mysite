8957f26b21a2e5cba06992bcba53011c1839bce5
DrShams <sithis02@mail.ru>
2025-02-02 17:23:27 +0300

    add fixes

--- mysite/settings.py
+++ mysite/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django.contrib.humanize',
 
     # Extensions - installed with requirements.txt
     'django_extensions', 
